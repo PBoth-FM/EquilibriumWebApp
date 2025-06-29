7446460eff9cfe7654870a2b0d3c6ebc8fd9ff1a
P Bothwell <pbothwell@fastmail.com>
2025-06-29 15:10:49 -0700

    Move underline much closer to bottom of 'works'

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
             Meditation that{' '}
             <span className="relative">
               works
-              <div className="absolute -bottom-1 left-0 right-0 h-1 bg-gradient-primary rounded-full"></div>
+              <div className="absolute left-0 right-0 h-1 bg-gradient-primary rounded-full" style={{ bottom: '-1px' }}></div>
             </span>{' '}
             for busy, analytical minds
           </h1>
